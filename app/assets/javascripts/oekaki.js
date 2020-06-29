7efbc7a6eb7ef68b654a09fd9b4b42daea99506e
isso <issoisso3@gmail.com>
2020-06-29 18:25:19 +0900

    save

--- app/assets/javascripts/oekaki.js
+++ app/assets/javascripts/oekaki.js
@@ -1,11 +1,12 @@
-var c = 0;
-var s = 1;
-var q = 0;
+let c = 0;
+let s = 1;
+let q = 0;
 
 function setup(){
   createCanvas(q);
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(window.innerWidth - 100, windowHeight - 100);
 }
+
 function draw(){
   stroke(c);
   strokeWeight(s);
@@ -14,32 +15,45 @@ function draw(){
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
+
 function keyPressed(){
   if (key == "s"){
     q = createCanvas(400,400);
   }
+
   if (key == "m"){
     q = createCanvas(700,700);
   }
+
   if (key == "l"){
     q = createCanvas(windowWidth, windowHeight);
   }
+
   if (key == '3'){
     s = 20;
   }
+
   if (key == '2'){
     s = 10;
   }
+
   if (key == '1'){
     s = 1;
   }
+
   if (key == 'r'){
     c = "red";
   }
+
   if (key == 'b'){
     c = "blue";
   }
+  
   if(key == 'y'){
     c = "yellow";
+  }
+
+  if(key == 'q') {
+    saveCanvas('myCanvas', 'jpg');
   }
 }
